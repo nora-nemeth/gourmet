fb8e2bff1654594f22237b458c2c5d918b55d678
Nora Nemeth <nemethnora1996@gmail.com>
2021-01-21 21:00:00 +0100

    Create application skeleton

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Hello, I am Gourmet!</h1>
+				<button className='btn btn-secondary btn-sm'>Bootstrap integration test</button>
+			</div>
+		);
+	}
+}
+
+export default App;
+
