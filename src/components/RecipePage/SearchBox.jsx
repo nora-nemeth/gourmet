c0d0ff86d1a59435939a7956415df20bd789a0dc
nora-nemeth <nemethnora1996@gmail.com>
2021-02-02 15:03:27 +0100

    Create search function

--- src/components/RecipePage/SearchBox.jsx
+++ src/components/RecipePage/SearchBox.jsx
@@ -1,18 +1,43 @@
 import React, { Component } from "react";
 import { Form, FormControl } from "react-bootstrap";
+import jsonData from "./RecipeCard/jsonData.json";
+import {useState} from 'react';
 
-class RecipePageSearchBox extends Component {
-  render() {
-    return (
-      <Form className="flex-grow-1 pt-3">
-        <FormControl
-          type="text"
-          placeholder="Mit főznél ma?"
-          className="bg-white text-dark pl-4 shadow-none"
-        />
-      </Form>
-    );
+function RecipePageSearchBox() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const onSubmit = e => {
+    e.preventDefault();
   }
+
+  const onChange = (event) => {
+    setSearchTerm(event.target.value);
+  }
+
+  return (
+    <Form className="flex-grow-1 pt-3" onSubmit={onSubmit}>
+      <FormControl
+        type="text"
+        placeholder="Mit főznél ma?"
+        className="bg-white text-dark pl-4 shadow-none"
+        autoComplete="off"
+        onChange={onChange}
+      />
+      {jsonData
+        .filter((value) => {
+          if (searchTerm == "") {
+            return value;
+          } else if (
+            value.name.toLowerCase().includes(searchTerm.toLowerCase())
+          ) {
+            return value;
+          }
+        })
+        .map((value, key) => {
+          return <div key={key}>{value.name}</div>;
+        })}
+    </Form>
+  );
 }
 
 export default RecipePageSearchBox;
